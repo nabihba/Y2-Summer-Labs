01bd2a1c618f09ea0bc23c5b5260426bca57f6ee
nabihba <nabihbarakat1234@gmail.com>
2024-07-22 18:29:19 +0300

    bombo

--- Auth Lab/app.py
+++ Auth Lab/app.py
@@ -10,12 +10,13 @@ Config = {
     "messagingSenderId": "355323432502",
     "appId": "1:355323432502:web:a87b3964c20ec3c997d297",
     "measurementId": "G-296NTRVJXC",
-    "databaseURL": ""
+    "databaseURL": "https://authentication-lab-76c53-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.config['SECRET_KEY'] = 'Your_secret_string'
 firebase = pyrebase.initialize_app(Config)
+db = firebase.database()
 auth = firebase.auth()
 
 @app.route("/home", methods=["GET", "POST"])
@@ -24,47 +25,55 @@ def home():
         return redirect(url_for('signin'))
     
     if request.method == 'POST':
-        quote = request.form['quote']
-        speaker = request.form['speaker']
+        text = request.form['quote']
+        said_by = request.form['speaker']
         info = request.form['info']
-        if 'quotes' not in login_session:
-            login_session['quotes'] = []
-        login_session['quotes'].append({
-            'quote': quote,
-            'speaker': speaker,
-            'info': info
-        })
-        login_session.modified = True
+        
+        quote = {
+            'text': text,
+            'said_by': said_by,
+            'info': info,
+            'uid': login_session['user']['localId']
+        }
+        
+        db.child("Quotes").push(quote)
         return redirect(url_for('thanks'))
     
     return render_template('home.html', email=login_session['user']['email'])
 
 @app.route("/display")
 def display():
-    print(login_session['quotes'] == [])
-    if 'quotes' not in login_session:
-        login_session['quotes'] = []
-    return render_template('display.html', quotes=login_session['quotes'])
-
+    quotes_dict = db.child("Quotes").get().val()
+    return render_template('display.html', quotes=quotes_dict)
 
 @app.route("/thanks")
 def thanks():
     return render_template('thanks.html')
 
-
-
 @app.route("/", methods=["GET", "POST"])
 def signup():
     if request.method == 'POST':
+        full_name = request.form['full_name']
+        username = request.form['username']
         email = request.form['email']
         password = request.form['password']
         try:
             user = auth.create_user_with_email_and_password(email, password)
             login_session['user'] = user
             login_session['quotes'] = []
+
+            user_data = {
+                'full_name': full_name,
+                'email': email,
+                'username': username
+            }
+            
+            uid = login_session['user']['localId']
+            db.child("Users").child(uid).set(user_data)
+
             return redirect(url_for('home'))
-        except Exception as e:
-            error_message = str(e)
+        except:
+            error_message = "Authentication failed"
             return render_template("error.html", error_message=error_message)
     return render_template("signup.html")
 
@@ -79,8 +88,8 @@ def signin():
             if 'quotes' not in login_session:
                 login_session['quotes'] = []
             return redirect(url_for('home'))
-        except Exception as e:
-            error_message = str(e)
+        except:
+            error_message = "Authentication failed"
             return render_template("error.html", error_message=error_message)
     return render_template("signin.html")
 
